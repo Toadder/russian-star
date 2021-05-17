0255621d972c3e32ea58187ff26742ba33cbb4aa
Toadder <zheka20042006@gmail.com>
2021-05-18 00:09:58 +1000

    3.0

--- js/main.js
+++ js/main.js
@@ -20,13 +20,6 @@ $(document).ready(function () {
   let introSlider = $(".intro__slider");
   let introProgressLine = $(".intro__slider-line");
 
-  // NAV SCROLL
-  $("a[href^='#']").click(function () {
-    var _href = $(this).attr("href");
-    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
-    return false;
-  });
-
   // SCROLL OF NEWS-HEADER
   if (!window.matchMedia("(max-width: 1030px)").matches) {
     $(window).on("scroll", () => {
